f26e486afd367657d8a9732f505b1aabc82662c8
Eric Petersen <eric@huskers.unl.edu>
2013-06-24 17:30:36 -0500

    got sign up and login/logout working

--- account/views.py
+++ account/views.py
@@ -1,7 +1,12 @@
+from django.db import IntegrityError
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
+from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate
+from django.contrib.auth import login as dj_login
+from django.contrib.auth import logout as dj_logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from djarzeit.context import ArZeitContext
@@ -15,10 +20,11 @@ def login(request):
     if request.POST:
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(email=email, password=password)
+        user = authenticate(username=email, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
+                dj_login(request, user)
+                return redirect('timers')
             else:
                 messages.error(request, 'Your account is inactive.')
         else:
@@ -31,9 +37,26 @@ def login(request):
 
 def new_account(request):
     if request.POST:
-        name = request.POST.get('name')
+        name_first = request.POST.get('name_first')
+        name_last = request.POST.get('name_last')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.first_name = name_first
+            user.last_name = name_last
+        except IntegrityError as e:
+            messages.error(request, e)
+            return redirect('home')
+        try:
+            user.full_clean()
+        except ValidationError as e:
+            for err in e.message_dict:
+                messages.error(request, err)
+        else:
+            user.save()
+            messages.success(request, 'New user created! Please log in.')
+        return redirect('home')
     else:
         context = AccountContext(request, {})
         return render_to_response('account/new.html', {}, context)
@@ -41,7 +64,7 @@ def new_account(request):
 
 @login_required
 def logout(request):
-    logout(request, request.user)
+    dj_logout(request)
     messages.success(request, 'Successfully logged out.')
     return redirect('home')
 
