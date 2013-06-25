e670d446a59660042b5fd8798088b500300957e1
Eric Petersen <eric@huskers.unl.edu>
2013-06-25 00:23:26 -0500

    adding active timers to context

--- djarzeit/context.py
+++ djarzeit/context.py
@@ -1,13 +1,20 @@
 from django.template import RequestContext
 from django.utils.timezone import now
 
+from timers.models import Timer
+
 
 class ArZeitContext(RequestContext):
     app = None
 
     def __init__(self, request, context, **kwargs):
+        active_timers = Timer.objects.filter(
+            category__user=request.user.id,
+            active=True
+        )
         context.update({
             'app': self.app,
             'server_time': now(),
+            'active_timers': active_timers,
         })
         super().__init__(request, context, **kwargs)
