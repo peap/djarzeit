82a1c5ec120b9f81a1918736e6e32de77af49866
Eric Petersen <eric@huskers.unl.edu>
2014-07-15 23:18:24 -0500

    Not doing user-specific things when we do not have a user

--- djarzeit/context.py
+++ djarzeit/context.py
@@ -13,17 +13,22 @@ class ArZeitContext(RequestContext):
     extra_js = ()
 
     def __init__(self, request, context, **kwargs):
-        active_timers = Timer.objects.filter(
-            category__user=request.user.id,
-            active=True
-        )
+        if request.user.is_authenticated():
+            active_timers = Timer.objects.filter(
+                category__user=request.user,
+                active=True
+            )
+            server_time = get_server_time_str(request.user)
+        else:
+            active_timers = []
+            server_time = now()
         extra_css = list(self.extra_css) + list(kwargs.pop('extra_css', ()))
         extra_js = list(self.extra_js) + list(kwargs.pop('extra_js', ()))
 
         context.update({
             'active_tab': self.active_tab,
             'auto_refresh': self.auto_refresh,
-            'server_time': get_server_time_str(request.user),
+            'server_time': server_time,
             'active_timers': active_timers,
             'path': request.path,
             'extra_css': extra_css,
