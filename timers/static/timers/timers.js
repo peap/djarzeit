34a873e861cf3fc1f8129c46bd571dc063d95b35
Eric Petersen <eric@huskers.unl.edu>
2014-07-21 17:13:59 -0500

    Simplifying panel collapse stuff

--- timers/static/timers/timers.js
+++ timers/static/timers/timers.js
@@ -1,36 +1,5 @@
 $(document).ready(function () {
 
-    $('#toggle_all').on('click', function (e) {
-        var $button = $(this);
-        var old_text = $button.text();
-        if (old_text.indexOf('Show') === -1){
-            $('.accordion-body').collapse('hide');
-            new_text = old_text.replace('Hide', 'Show');
-        } else {
-            $('.accordion-body').collapse('show');
-            new_text = old_text.replace('Show', 'Hide');
-        }
-        $button.text(new_text);
-    });
-
-    $('.panel-body').collapse();
-    $('.panel-heading').on('click', function (e) {
-        var $panel = $(this).closest('.panel');
-        var $panel_body = $panel.find('.panel-body');
-        var $clicked_col = $(e.target).closest('div');
-        if ($clicked_col.attr('data-toggle') === 'add_timer') {
-            var $target = $($clicked_col.attr('data-target'));
-            if ($target.is(':visible')) {
-                $target.addClass('hide');
-            } else {
-                $target.removeClass('hide');
-                $target.find('input[name=timer_name]').focus();
-            }
-        } else {
-            $panel_body.collapse('toggle');
-        }
-    });
-
     $('form.startstop-form').on('submit', function (e) {
         e.preventDefault();
         var $form = $(this);
