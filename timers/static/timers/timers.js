03b6632462d18d7af515d711656d2696302abd33
Eric Petersen <eric@huskers.unl.edu>
2014-07-24 16:14:07 -0500

    Only modifying startstop buttons in the JS now

--- timers/static/timers/timers.js
+++ timers/static/timers/timers.js
@@ -11,9 +11,11 @@ $(document).ready(function () {
             var $startstop_btn = $form.find('button[type="submit"]');
             var old_text = $startstop_btn.text().trim();
             if (old_text === 'Start') {
-                $form.closest('.root-category-panel').find('button')
+                $form.closest('.root-category-panel')
+                     .find('form.startstop-form button')
                      .removeClass('btn-danger').addClass('btn-success');
-                $form.closest('.root-category-panel').find('button[type="submit"]')
+                $form.closest('.root-category-panel')
+                     .find('form.startstop-form button[type="submit"]')
                      .text('Start');
                 $form_btns.removeClass('btn-success').addClass('btn-danger');
                 $startstop_btn.text('Stop');
