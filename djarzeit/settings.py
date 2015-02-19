b4c2a2962101f818b2840590ee281c71df2c4085
Eric Petersen <eric@huskers.unl.edu>
2015-02-18 22:52:04 -0600

    Switching to Django 1.7.4 and Django Migrations

--- djarzeit/settings.py
+++ djarzeit/settings.py
@@ -117,8 +117,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'south',
-
     'account',
     'categories',
     'reports',
--- timers/migrations/0001_initial.py
+++ timers/migrations/0001_initial.py
@@ -1,131 +1,49 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    depends_on = (
+    dependencies = [
         ('categories', '0001_initial'),
-    )
+    ]
 
-    def forwards(self, orm):
-        # Adding model 'Timer'
-        db.create_table('timers_timer', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['categories.Category'])),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('description', self.gf('django.db.models.fields.CharField')(null=True, blank=True, max_length=1000)),
-            ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('timers', ['Timer'])
-
-        # Adding model 'Interval'
-        db.create_table('timers_interval', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('timer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['timers.Timer'])),
-            ('week', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
-            ('year', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
-            ('start', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('end', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
-            ('notes', self.gf('django.db.models.fields.CharField')(null=True, blank=True, max_length=1000)),
-        ))
-        db.send_create_signal('timers', ['Interval'])
-
-        # Adding M2M table for field tags on 'Interval'
-        m2m_table_name = db.shorten_name('timers_interval_tags')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('interval', models.ForeignKey(orm['timers.interval'], null=False)),
-            ('tag', models.ForeignKey(orm['tags.tag'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['interval_id', 'tag_id'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Timer'
-        db.delete_table('timers_timer')
-
-        # Deleting model 'Interval'
-        db.delete_table('timers_interval')
-
-        # Removing M2M table for field tags on 'Interval'
-        db.delete_table(db.shorten_name('timers_interval_tags'))
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True', 'symmetrical': 'False'})
-        },
-        'auth.permission': {
-            'Meta': {'object_name': 'Permission', 'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)"},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'max_length': '75'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True', 'symmetrical': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True', 'symmetrical': 'False'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'categories.category': {
-            'Meta': {'object_name': 'Category'},
-            'description': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '1000'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'null': 'True', 'blank': 'True', 'to': "orm['categories.Category']"}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'object_name': 'ContentType', 'db_table': "'django_content_type'", 'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'tags.tag': {
-            'Meta': {'object_name': 'Tag'},
-            'description': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '1000'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
-        },
-        'timers.interval': {
-            'Meta': {'object_name': 'Interval'},
-            'end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'notes': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '1000'}),
-            'start': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['tags.Tag']", 'db_index': 'True', 'blank': 'True', 'null': 'True', 'symmetrical': 'False'}),
-            'timer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['timers.Timer']"}),
-            'week': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
-            'year': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'})
-        },
-        'timers.timer': {
-            'Meta': {'object_name': 'Timer'},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['categories.Category']"}),
-            'description': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '1000'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['timers']
+    operations = [
+        migrations.CreateModel(
+            name='Interval',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('week', models.IntegerField(db_index=True, verbose_name='Week')),
+                ('year', models.IntegerField(db_index=True, verbose_name='Year')),
+                ('start', models.DateTimeField(auto_now_add=True, verbose_name='Start Time')),
+                ('end', models.DateTimeField(blank=True, null=True, verbose_name='End Time')),
+                ('notes', models.CharField(blank=True, max_length=1000, null=True, verbose_name='Notes')),
+            ],
+            options={
+                'ordering': ['-start'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Timer',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, verbose_name='Name')),
+                ('active', models.BooleanField(default=False, verbose_name='Active')),
+                ('archived', models.BooleanField(default=False, verbose_name='Archived')),
+                ('category', models.ForeignKey(to='categories.Category')),
+            ],
+            options={
+                'ordering': ['name'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='interval',
+            name='timer',
+            field=models.ForeignKey(to='timers.Timer'),
+            preserve_default=True,
+        ),
+    ]
