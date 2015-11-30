c75797566b2aca0ae05c635938004d6b7195c0dd
Eric Petersen <eric@huskers.unl.edu>
2015-11-29 22:09:52 -0800

    Add outline of Timeline page - everything but the data

--- timers/static/timers/timeline.js
+++ timers/static/timers/timeline.js
@@ -1,3 +1,56 @@
 $(document).ready(function(){
 
+    // Setup SVG container
+    var nRows = window.timelineData.nCategories;
+    var height = 150 + 50 * nRows;
+    var width = $('#timeline').width();
+    var margin = {
+        'top': 15,
+        'bottom': 25,
+        'left': 0.05 * width,
+        'right': 0.05 * width,
+    }
+    var h = height - margin.top - margin.bottom;
+    var w = width - margin.right - margin.left;
+
+    var svg = d3.select('#timeline')
+        .append('svg')
+        .attr('height', height)
+        .attr('width', width);
+
+    // Add title
+    var title = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .attr('class', 'title')
+      .append('text')
+       .attr('x', (w / 2))
+       .attr('y', margin.top)
+       .attr('text-anchor', 'middle')
+       .text('Timeline of Intervals on ' + window.timelineData.dateStr);
+
+    // Add axis
+    var timeScale = d3.time.scale()
+        .domain([window.timelineData.minHour, window.timelineData.maxHour])
+        .range([0, w]);
+
+    var timeAxis = d3.svg.axis()
+        .scale(timeScale)
+        .tickFormat(d3.time.format('%I:%M %p'))
+        .orient('bottom');
+
+    svg.append('g')
+        .attr('class', 'time-axis')
+        .attr('transform',
+              'translate(' + margin.left + ',' + (height - margin.bottom - 50) + ')')
+        .call(timeAxis)
+      .selectAll('text')
+        .attr('x', 9)
+        .attr('y', 0)
+        .attr('dy', '.35em')
+        .attr('transform', 'rotate(90)')
+        .style('text-anchor', 'start');
+
+    // TODO: Add data
+
+
 });
--- timers/views.py
+++ timers/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime, time, timedelta
 import json
 
 from django.contrib import messages
@@ -11,7 +12,8 @@ from categories.views import CategoryDetailView
 from core.context import ArZeitContext
 from core.json import get_base_json_data
 from core.views import ArZeitBaseDetailView, ArZeitTemplateView
-from timers.models import Timer
+from reports.utils import get_report_date
+from timers.models import Timer, Interval
 
 
 class TimersContext(ArZeitContext):
@@ -161,3 +163,82 @@ class Delete(TimerDetailView):
 class Timeline(ArZeitTemplateView):
     context_class = TimelineContext
     template_name = 'timers/timeline.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        data_date = get_report_date(self.request)
+        data = self.get_timeline_data(data_date)
+        ctx.update({
+            'report_date': data_date,
+            'data': data,
+        })
+        return ctx
+
+    def get_timeline_data(self, data_date):
+        interval_data = {}
+        min_time = None
+        min_hour = None
+        max_time = None
+        max_hour = None
+
+        user_intervals = Interval.user_intervals(self.request.user)
+        y, m, d = data_date.year, data_date.month, data_date.day
+        tz = data_date.tzinfo
+        min_datetime = datetime(y, m, d, 0, 0, 0, 0, tz)
+        max_datetime = datetime(y, m, d, 23, 59, 59, 999999, tz)
+        intervals = (
+            user_intervals
+                .filter(start__range=(min_datetime, max_datetime))
+                .order_by('start')
+        )
+
+        nowtz = now().astimezone(tz=tz)
+
+        for interval in intervals:
+            root_cat = interval.timer.category.root_parent.name
+            start = interval.start.astimezone(tz=tz)
+            end = interval.end.astimezone(tz=tz) if interval.end else nowtz
+
+            if min_time is None or start < min_time:
+                min_time = start
+            if max_time is None or (end and end > max_time):
+                max_time = end
+            if root_cat not in interval_data:
+                interval_data[root_cat] = {
+                    'category_name': root_cat,
+                    'intervals': [],
+                }
+            interval_data[root_cat]['intervals'].append({
+                'interval_id': interval.id,
+                'timer_id': interval.timer.id,
+                'timer_name': interval.timer.name,
+                'start': start,
+                'end': end,
+            })
+
+        if min_time is None:
+            min_hour = time(8, 0, 0, 0)
+        else:
+            h, m, s, ms = [
+                getattr(min_time, x)
+                for x in ['hour', 'minute', 'second', 'microsecond']
+            ]
+            min_hour = time(h, 0, 0, 0)
+
+        if max_time is None:
+            max_hour = time(17, 0, 0, 0)
+        else:
+            # cap at midnight
+            hour_later = max([max_time, max_time + timedelta(hours=1)])
+            # TODO: handle intervals extending past midnight...
+            h, m, s, ms = [
+                getattr(hour_later, x)
+                for x in ['hour', 'minute', 'second', 'microsecond']
+            ]
+            max_hour = time(h, 0, 0, 0)
+
+        return {
+            'interval_data': interval_data,
+            'min_hour': min_hour,
+            'max_hour': max_hour,
+        }
